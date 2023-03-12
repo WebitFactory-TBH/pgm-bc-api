3cedb204cf0254bb061e2dc52997d59d067dc7e8
Drenea Marius <dreneamarius@gmail.com>
2023-03-12 22:10:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // pdfGeneratorService configs
-export const GOTENBERG_URL = "http://173.249.22.67:3333"
+export const GOTENBERG_URL = "http://url"
 
 // mailService configs - just for demo purpose will be stored here.
-export const MAIL_AUTH_EMAIL = "wfytbh@gmail.com"
-export const MAIL_AUTH_PASSWORD = "kuidamerlhihjgbk"
+export const MAIL_AUTH_EMAIL = "email@email.com"
+export const MAIL_AUTH_PASSWORD = "pass"
 export const MAIL_FROM_NAME = "Pagament.io"
 export const MAIL_FROM_EMAIL = "noreply@pagament.io"
