73d0f244a0fc56491ffc841ca2a412b6d546d10e
Marius <dreneamarius@gmail.com>
2023-03-12 09:54:59 +0200

    Fix

--- mailService.js
+++ mailService.js
@@ -1,8 +1,36 @@
-import axios from 'axios';
-// import { MONGO_DB_URI, CONTRACT_ADDRESS, BLAST_API_RPC_ENDPOINT } from "./config.js";
+import { MAIL_AUTH_EMAIL, MAIL_AUTH_PASSWORD, MAIL_FROM_NAME, MAIL_FROM_EMAIL } from "./config.js";
+const nodemailer = require('nodemailer');
 
 export class MailService {
     async sendEmai(email, content){
-        "email trimis cu success";  
+        return await this.#send(email, content);
+    }
+
+    async #send(email, content){
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: MAIL_AUTH_EMAIL,
+              pass: MAIL_AUTH_PASSWORD
+            }
+        });
+          
+        const mailOptions = {
+            from: {
+                name: MAIL_FROM_NAME,
+                address: MAIL_FROM_EMAIL
+            },
+            to: email,
+            subject: 'New Payment Link Created',
+            text: content
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                return error;
+            } else {
+                return true;
+            }
+        });
     }
 }
